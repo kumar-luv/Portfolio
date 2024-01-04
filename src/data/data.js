3fab8672200598e903c82436dfd4d05df65a65b3
kumar-luv <luvchaudhary4566@gmail.com>
2024-01-05 03:39:53 +0530

    portfolio created

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+
+import NetflixGPT from '../assets/projects/NetflixGPT.png'
+import Video from '../assets/projects/Video.png'
+import Razorpay from '../assets/projects/Razorpay.png'
+import Password from '../assets/projects/Password.png'
+import Top from '../assets/projects/Top.png'
+import Random from '../assets/projects/Random.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Video Streaming App",
+        image:Video,
+        github:"https://github.com/kumar-luv/Youtube-clone",
+        live:"https://video-streaming-app-beryl.vercel.app/",
+    },
+    {
+        id:2,
+        name:"NetflixGPT",
+        image:NetflixGPT,
+        github:"https://github.com/kumar-luv/Netflix-Clone",
+        live:"https://netflixgpt-seven.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Razorpay",
+        image:Razorpay,
+        github:"https://github.com/kumar-luv/Razorpay_clone",
+        live:"https://merry-crepe-8b2b85.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Rondam gif",
+        image:Random,
+        github:"https://github.com/kumar-luv/random-gif-starter",
+        live:"https://random-gif-starter-rho.vercel.app/",
+    },
+    {
+        id:5,
+        name:"Top Course",
+        image:Top,
+        github:"https://github.com/kumar-luv/Top-course-project",
+        live:"https://top-course-project-sigma.vercel.app/",
+    },
+    {
+        id:6,
+        name:"Password Generator",
+        image:Password,
+        github:"https://github.com/kumar-luv/Password-generator",
+        live:"https://meek-florentine-09d3b7.netlify.app/",
+    },
+
+
+
+]
